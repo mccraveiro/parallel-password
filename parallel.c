c8fd40bb229a5d7780f85231c4f8bafa70f49048
Mateus Craveiro <mccraveiro@gmail.com>
2015-09-10 21:57:28 -0300

    Parallel starting code

--- parallel.c
+++ parallel.c
@@ -4,34 +4,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #if defined(__linux__)
 #include <crypt.h>
 #endif
 
 #define HASH_SIZE 256
+#define NTHREADS 2
 
-/*
- * @brief Generate a Hash from a password string
- * @param password password string
- * @param hash generated Hash
- */
-char *generatePasswordHash(const char *password);
-
-/*
- * @brief Set 'password' to the next value to be tested
- * @param password password string
- * @return -1 if there are no more possible values
- */
-int getNextPassword(char *password);
-
-/*
- * @brief compare password with target_hash
- * @param target_hash string
- * @param password string
- * @return 1 if equal, otherwise 0.
- */
-int ComparePassword(const char *target_hash, const char *password);
+typedef struct {
+  char *target_hash;
+  char *password;
+  int ntries;
+} thread_data;
 
 /*
  * @brief number of chars used on password
@@ -39,75 +25,127 @@ int ComparePassword(const char *target_hash, const char *password);
 int passwordLength = 0;
 
 /*
- * @brief string used to generate password
+ * @brief Generate a Hash from a password string
+ * @param password password string
+ * @param hash generated Hash
  */
-char *password;
+char *generate_password_hash(const char *password) {
+	return crypt(password, "aa");
+}
+
+/*
+ * @brief Set 'password' to the next value to be tested
+ * @param password password string
+ * @return -1 if there are no more possible values
+ */
+int get_next_password(char *password) {
+  // Increment password chars from right to left Ex.: 0000, 0001, 0002 ... 0010...
+	int index = passwordLength - 1;
+
+	while (index >= 0) {
+    // just increase last digit
+		if (password[index] != '9') {
+			password[index]++;
+			break;
+		}
+
+    // no more possible passwords
+    if (index == 0) {
+      return 0;
+    }
+
+    // carry 1 to next digit
+    password[index] = '0';
+		index--;
+	}
+
+	return 1;
+}
+
+/*
+ * @brief compare password with target_hash
+ * @param target_hash string
+ * @param password string
+ * @return 1 if equal, otherwise 0.
+ */
+int compare_password(const char *target_hash, const char *password) {
+	// char generatedHash[HASH_SIZE + 1];
+	// strcpy(generatedHash, generate_password_hash(password));
+
+	// compare generated hash with target hash
+	if (strcmp(target_hash, generate_password_hash(password)) == 0) {
+		return 1;
+	}
+
+	return 0;
+}
+
+void *compare_password_pthread(void *data_ptr) {
+  int i;
+  thread_data *data = (thread_data *)data_ptr;
+
+  for (i = 0; i < data->ntries; i++) {
+    // printf("%s\n", data->password);
+
+    if (compare_password(data->target_hash, data->password)) {
+      printf("%s - %s\n", data->target_hash, data->password);
+      break;
+    }
+
+    if (!get_next_password(data->password)) {
+      break;
+    }
+  }
+
+  return NULL;
+}
 
 int main(int argc, char *argv[]) {
-	int success = 0;
-
-	// time spent running the program
+  // time spent running the program
 	clock_t start = 0;
 	clock_t finish = 0;
 
-	// ensure the program got the right arguments
+  thread_data *data[NTHREADS];
+  pthread_t thread[NTHREADS];
+
+  // ensure the program got the right arguments
 	if (argc < 3) {
 		printf("Usage: %s <password length> <target hash>", argv[0]);
 		return 1;
 	}
 
 	passwordLength = atoi(argv[1]);
-	password = malloc(sizeof(char) * (passwordLength + 1));
 
-  // set initial password to '0' chars
-	memset(password, '0', passwordLength);
-	password[passwordLength] = '\0';
+  for (int i = 0; i < NTHREADS; i++) {
+    data[i] = malloc(sizeof(thread_data));
+    data[i]->target_hash = strdup(argv[2]);
+    data[i]->password = malloc(sizeof(char) * (passwordLength + 1));
+    data[i]->ntries = 5000;
+
+    // set initial password to '0' chars
+  	memset(data[i]->password, '0', passwordLength);
+  	data[i]->password[passwordLength] = '\0';
+
+    // TODO: split passwords between threads
+    if (i == 1) {
+      data[i]->password[0] = '5';
+    }
+  }
 
 	// start timer
 	start = clock();
 
-	// test all possile passwords
-	while ((success = ComparePassword(argv[2], password)) == 0 && getNextPassword(password) != -1);
+  for (int i = 0; i < NTHREADS; i++) {
+    pthread_create(&thread[i], NULL, compare_password_pthread, data[i]);
+  }
 
-	// stop timer
+  for (int i = 0; i < NTHREADS; i++) {
+    pthread_join(thread[i], NULL);
+  }
+
+	// stop timer and print result
 	finish = clock();
-
-	if (success) {
-		printf("%f\n", (float)(finish - start) / CLOCKS_PER_SEC);
-	}
+	printf("%f\n", (float)(finish - start) / CLOCKS_PER_SEC);
 
 	return 0;
-}
-
-int ComparePassword(const char *hashAlvo, const char *password) {
-	// char generatedHash[HASH_SIZE + 1];
-	// strcpy(generatedHash, generatePasswordHash(password));
-
-	// compare generated hash with target hash
-	if (!strcmp(hashAlvo, generatePasswordHash(password))) {
-		return 1;
-	}
-
-	return 0;
-}
-
-int getNextPassword(char *password) {
-  // Increment password chars from right to left Ex.: 0000, 0001, 0002 ... 0010...
-	int index = passwordLength - 1;
-
-	while (index >= 0) {
-		if (password[index] != '9') {
-			password[index]++;
-			break;
-		}
-
-    password[index] = '0';
-		index--;
-	}
-
-	return index;
-}
-
-char *generatePasswordHash(const char *password) {
-	return crypt(password, "aa");
 }
